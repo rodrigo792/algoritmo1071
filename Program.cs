0fc0a0bb0733a37a11c3dfd941c4accd073fe5c9
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-04 17:43:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace algoritmo1071
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+
+            string[] entrada = Console.ReadLine().Split(' ');
+            int x = int.Parse(entrada[0]);
+            int y = int.Parse(entrada[6]);
+
+  
+            if (x > y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+
+            int soma = 0;
+
+            // Somar os números ímpares entre X e Y
+            for (int i = x + 1; i < y; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            // Exibir a soma dos números ímpares
+            Console.WriteLine(soma);
+        }
+    }
+}
